04516707c8343dae103e8995bda85600e585db0d
phuquocchamp <hoangtanphuquoc@gmail.com>
2022-11-20 17:55:21 +0700

    Update all files

--- app.js
+++ app.js
@@ -21,30 +21,10 @@ const next = document.querySelector('#next');
         });
 
         const playList = [{
-            artist: 'Post Malone',
-            cover: 'https://yildirimzlm.s3.us-east-2.amazonaws.com/post-malone-2.jpeg',
-            musicName: 'Rockstar ft. 21 Savage',
-            musicPath: `https://yildirimzlm.s3.us-east-2.amazonaws.com/Post+Malone+-+rockstar+ft.+21+Savage+(Official+Audio).mp3`
-        }, {
-            artist: 'Unlike Pluto',
-            cover: 'https://yildirimzlm.s3.us-east-2.amazonaws.com/unlike-pluto.jpeg',
-            musicName: 'No Scrubs ft. Joanna Jones',
-            musicPath: `https://yildirimzlm.s3.us-east-2.amazonaws.com/Unlike+Pluto+-+No+Scrubs+ft.+Joanna+Jones+(Cover).mp3`
-        }, {
-            artist: 'Post Malone',
-            cover: 'https://yildirimzlm.s3.us-east-2.amazonaws.com/circles.jpeg',
-            musicName: 'Circles',
-            musicPath: `https://yildirimzlm.s3.us-east-2.amazonaws.com/Post+Malone+-+Circles+(Lyrics).mp3`
-        }, {
-            artist: 'Lil Nas X',
-            cover: 'https://yildirimzlm.s3.us-east-2.amazonaws.com/montero.jpeg',
-            musicName: 'MONTERO (Call Me By Your Name)',
-            musicPath: `https://yildirimzlm.s3.us-east-2.amazonaws.com/Lil+Nas+X+-+MONTERO+(Call+Me+By+Your+Name)+(Lyrics).mp3`
-        }, {
-            artist: 'Post Malone',
-            cover: 'https://yildirimzlm.s3.us-east-2.amazonaws.com/post-malone-1.jpeg',
-            musicName: 'Better Now',
-            musicPath: `https://yildirimzlm.s3.us-east-2.amazonaws.com/Post+Malone+-+Better+Now.mp3`
+            artist: 'Son Tung - MTP',
+            cover: './img/2.jpg',
+            musicName: 'Noi Nay Co Anh',
+            musicPath: './audio/Noi-Nay-Co-Anh-Masew-Bootleg-Son-Tung-M-TP-Masew.mp3'
         }]
 
         const playMusic = () => {
